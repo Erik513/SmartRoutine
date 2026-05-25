e1e2dc5e3b0abaa2f49cb1638916f90e815f3cfa
Erik513 <erni-wink@web.de>
2026-05-25 17:45:30 +0200

    Refactor BorderlessResizableForm

--- SmartRoutine/UI/Forms/BorderlessResizableForm.cs
+++ SmartRoutine/UI/Forms/BorderlessResizableForm.cs
@@ -1,91 +1,106 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SmartRoutine.UI.Forms
 {
     public partial class BorderlessResizableForm : Form
     {
-        private const int HTLEFT = 10;
-        private const int HTRIGHT = 11;
-        private const int HTTOP = 12;
-        private const int HTTOPLEFT = 13;
-        private const int HTTOPRIGHT = 14;
-        private const int HTBOTTOM = 15;
-        private const int HTBOTTOMLEFT = 16;
-        private const int HTBOTTOMRIGHT = 17;
-        private const int RESIZE_BORDER = 6; // Size of "resizable" Area
+        private const int WmNcHitTest = 0x84;
 
-        private bool _isResizing = false;
+        private const int HtLeft = 10;
+        private const int HtRight = 11;
+        private const int HtTop = 12;
+        private const int HtTopLeft = 13;
+        private const int HtTopRight = 14;
+        private const int HtBottom = 15;
+        private const int HtBottomLeft = 16;
+        private const int HtBottomRight = 17;
+
+        private const int ResizeBorder = 6;
+        private const int FormPadding = 2;
+
+        public BorderlessResizableForm()
+        {
+            ConfigureForm();
+        }
 
         protected override void WndProc(ref Message m)
         {
-            const int WM_NCHITTEST = 0x84;
-
             base.WndProc(ref m);
 
-            if (m.Msg == WM_NCHITTEST)
-            {
-                if (this.WindowState == FormWindowState.Maximized)
-                {
-                    m.Result = (IntPtr)0;
-                    return;
-                }
-                Point pos = PointToClient(new Point(m.LParam.ToInt32()));
-                if (pos.X < RESIZE_BORDER && pos.Y < RESIZE_BORDER)
-                    m.Result = (IntPtr)HTTOPLEFT;
-                else if (pos.X > ClientSize.Width - RESIZE_BORDER && pos.Y < RESIZE_BORDER)
-                    m.Result = (IntPtr)HTTOPRIGHT;
-                else if (pos.X < RESIZE_BORDER && pos.Y > ClientSize.Height - RESIZE_BORDER)
-                    m.Result = (IntPtr)HTBOTTOMLEFT;
-                else if (pos.X > ClientSize.Width - RESIZE_BORDER && pos.Y > ClientSize.Height - RESIZE_BORDER)
-                    m.Result = (IntPtr)HTBOTTOMRIGHT;
-                else if (pos.X < RESIZE_BORDER)
-                    m.Result = (IntPtr)HTLEFT;
-                else if (pos.X > ClientSize.Width - RESIZE_BORDER)
-                    m.Result = (IntPtr)HTRIGHT;
-                else if (pos.Y < RESIZE_BORDER)
-                    m.Result = (IntPtr)HTTOP;
-                else if (pos.Y > ClientSize.Height - RESIZE_BORDER)
-                    m.Result = (IntPtr)HTBOTTOM;
-            }
+            if (m.Msg != WmNcHitTest)
+                return;
+
+            if (WindowState == FormWindowState.Maximized)
+                return;
+
+            IntPtr hitTestResult = GetResizeHitTestResult(m);
+
+            if (hitTestResult != IntPtr.Zero)
+                m.Result = hitTestResult;
         }
-        protected override void OnResize(EventArgs e)
+
+        private void ConfigureForm()
         {
-            if (!_isResizing)
-            {
-                base.OnResize(e);
-            }
-            else
-            {
-                // Während des Resizings nur das Nötigste machen
-                this.Invalidate();
-            }
+            FormBorderStyle = FormBorderStyle.None;
+            DoubleBuffered = true;
+            Padding = new Padding(FormPadding);
+
+            SetStyle(
+                ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.ResizeRedraw,
+                true);
+
+            UpdateStyles();
         }
-        /// <summary>
-        /// Erstellt ein fensterloses Formular ohne Standard-Titelleiste, 
-        /// das dennoch in der Größe verändert werden kann.
-        /// </summary>
-        /// <remarks>
-        /// Die Klasse überschreibt die WndProc-Methode, um WM_NCHITTEST-Nachrichten abzufangen.
-        /// Dadurch wird ein 6 Pixel breiter Rand um das Formular als Resize-Bereich definiert,
-        /// der alle 8 Ziehpunkte (Ecken und Kanten) für die Größenänderung unterstützt.
-        /// </remarks>
-        public BorderlessResizableForm()
+
+        private IntPtr GetResizeHitTestResult(Message message)
         {
-            InitializeComponent();
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.DoubleBuffered = true;
-            this.SetStyle(ControlStyles.OptimizedDoubleBuffer |
-                         ControlStyles.AllPaintingInWmPaint |
-                         ControlStyles.ResizeRedraw, true);
-            this.Padding = new Padding(2);
+            Point screenPoint = GetPointFromLParam(message.LParam);
+            Point clientPoint = PointToClient(screenPoint);
+
+            bool left = clientPoint.X < ResizeBorder;
+            bool right = clientPoint.X > ClientSize.Width - ResizeBorder;
+            bool top = clientPoint.Y < ResizeBorder;
+            bool bottom = clientPoint.Y > ClientSize.Height - ResizeBorder;
+
+            if (left && top)
+                return (IntPtr)HtTopLeft;
+
+            if (right && top)
+                return (IntPtr)HtTopRight;
+
+            if (left && bottom)
+                return (IntPtr)HtBottomLeft;
+
+            if (right && bottom)
+                return (IntPtr)HtBottomRight;
+
+            if (left)
+                return (IntPtr)HtLeft;
+
+            if (right)
+                return (IntPtr)HtRight;
+
+            if (top)
+                return (IntPtr)HtTop;
+
+            if (bottom)
+                return (IntPtr)HtBottom;
+
+            return IntPtr.Zero;
+        }
+
+        private static Point GetPointFromLParam(IntPtr lParam)
+        {
+            int value = lParam.ToInt32();
+
+            int x = value & 0xFFFF;
+            int y = value >> 16;
+
+            return new Point(x, y);
         }
     }
 }
