c47a4ea4a0c5c9d83d70e7eacbb13f44acd93659
Erik513 <erni-wink@web.de>
2026-05-25 18:30:08 +0200

    Refactor AppSettings

--- SmartRoutine/UI/Helpers/AppSettings.cs
+++ SmartRoutine/UI/Helpers/AppSettings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SmartRoutine.UI.Helpers
+﻿namespace SmartRoutine.UI.Helpers
 {
     public static class AppSettings
     {
